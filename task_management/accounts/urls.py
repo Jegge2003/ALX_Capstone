05cbc946e7bc6d3a13c3247fbeb8e3a611cbd77f
Jegge2003 <jamesoappiah2003@gmail.com>
2025-04-01 11:21:52 +0000

    Updated urls, views, and serializers

--- task_management/accounts/urls.py
+++ task_management/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import RegisterView, LoginView
+from .views import CustomAuthToken
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
+    path('login/', CustomAuthToken.as_view(), name='token_auth'),
 ]
--- task_management/accounts/views.py
+++ task_management/accounts/views.py
@@ -4,6 +4,9 @@ from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
@@ -18,3 +21,9 @@ class LoginView(generics.GenericAPIView):
         user = self.get_serializer(data=request.data).validate(request.data)
         token, created = Token.objects.get_or_create(user=user)
         return Response({'token': token.key})
+    
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'user_id': token.user_id, 'username': token.user.username})
