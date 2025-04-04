098475db53a1d4fac199e70898de9f59ebcd3e6f
Jegge2003 <jamesoappiah2003@gmail.com>
2025-04-04 18:58:05 +0000

    Commented and modified URL

--- task_management/accounts/urls.py
+++ task_management/accounts/urls.py
@@ -1,10 +1,9 @@
 #Importing the necessary libraries
-from django.urls import path
+from django.urls import path #Used to define URL routes
 from .views import RegisterView, LoginView
 from .views import CustomAuthToken
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login', LoginView.as_view(), name='login'),
-    path('login/', CustomAuthToken.as_view(), name='token_auth'),
+    path('register/', RegisterView.as_view(), name='register'), #Mapping the URL register to the RegisterView class
+    path('login/', CustomAuthToken.as_view(), name='token_auth'), #Mapping the URL login to CustomAuthToken
 ]
