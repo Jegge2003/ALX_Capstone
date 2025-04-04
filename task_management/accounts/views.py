48aaa2325bc20d1319c6e4b0a2138e3dcb37d667
Jegge2003 <jamesoappiah2003@gmail.com>
2025-04-04 15:36:19 +0000

    Commented and modified views.py in the accounts app

--- task_management/accounts/views.py
+++ task_management/accounts/views.py
@@ -1,28 +1,34 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .serializers import RegisterSerializer, LoginSerializer
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics #For access to generic views like CreateAPIView and GenericAPIView
+from .serializers import RegisterSerializer, LoginSerializer #Serializers for user registration and login
+from rest_framework.authtoken.models import Token #Needed for login and token generation
+from rest_framework.response import Response #Required for all views that return data
+from rest_framework.permissions import AllowAny #Allows any user, even unauthenticated ones to access the endpoint
+from rest_framework.authtoken.views import ObtainAuthToken #For generating a token when a user logs in 
 
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
+    """
+    This view handles user registration/signup and makes it possible for even unauthenticated users to register.
+    """
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
 
 class LoginView(generics.GenericAPIView):
+    """
+    Custom login endpoint. Handles login and gives a token if credentials are valid
+    """
     serializer_class = LoginSerializer
     permission_classes = [AllowAny]
 
     def post(self, request):
-        user = self.get_serializer(data=request.data).validate(request.data)
-        token, created = Token.objects.get_or_create(user=user)
+        user = self.get_serializer(data=request.data).validate(request.data) #Running validation to ensure crediatials are correct
+        token, created = Token.objects.get_or_create(user=user) #Gives the user a token
         return Response({'token': token.key})
     
 class CustomAuthToken(ObtainAuthToken):
+    """
+    Provides a more detailed token response.
+    """
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         token = Token.objects.get(key=response.data['token'])
