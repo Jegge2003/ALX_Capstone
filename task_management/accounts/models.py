577a3e30d89d4cc6e1089174eec8d994ad3b621d
Jegge2003 <jamesoappiah2003@gmail.com>
2025-04-04 19:50:52 +0000

    Extended CustomUser model to include profile and bio, and also commented

--- task_management/accounts/models.py
+++ task_management/accounts/models.py
@@ -1,14 +1,13 @@
-
-"""
-Here we are building the CustomUser model which will be more like a user table inheriting all default fields of Django's AbstractUser.
-It will also have in addition bio and profile picture fields.
-"""
 #Importing necessary libraries
 from django.db import models #For building models which map to database tables
 from django.contrib.auth.models import AbstractUser
 
 # Creating a custom user model which extends Django's AbstractUser 
 class CustomUser(AbstractUser):
+    """
+    Here we are building the CustomUser model which will be more like a user table inheriting all default fields of Django's AbstractUser.
+    It will also have in addition bio and profile picture fields.
+    """
     #Adding bio and profile picture as the extended fields
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile/pics', blank=True, null=True)
