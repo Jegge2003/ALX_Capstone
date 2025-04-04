2cc57680583bd6d9bd8a5329291e3240f358115a
Jegge2003 <jamesoappiah2003@gmail.com>
2025-04-04 19:08:31 +0000

    Modified models and serializers for tasks app

--- task_management/tasks/models.py
+++ task_management/tasks/models.py
@@ -1,30 +1,37 @@
-from django.db import models
-from accounts.models import CustomUser
-from django.utils import timezone
+#Importing necessary libraries
+from django.db import models #Needed to define the database schema using python classes
+from accounts.models import CustomUser #Importing the CustomUser model from the accounts app
+from django.utils import timezone #To get the current time
 
 # Create your models here.
 class Task(models.Model):
+    """
+    Defines a new model (database table) named Task
+    """
+    #Allowed values for the status field
     STATUS_CHOICES = [
         ('Pending', 'Pending'),
         ('In Progress', 'In Progress'),
         ('Completed', 'Completed'),
     ]
 
+    #Allowed values for the priority field
     PRIORITY_CHOICES = [
         ('Low', 'Low'),
         ('Medium', 'Medium'),
         ('High', 'High'),
     ]
 
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=255, null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
-    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='Medium')
-    due_date = models.DateField(null=True, blank=True)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True) #Field linking each task to a user
+    title = models.CharField(max_length=255, null=True, blank=True) #Field storing the title of tasks
+    description = models.TextField(null=True, blank=True) #Field for storing more detailed description of the task
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending') #A dropdown/select field that must be one of the status choices
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='Medium') #A dropdown/select field that must be one of the priority choices
+    due_date = models.DateField(null=True, blank=True) #A date only field for task deadlines
+    created_at = models.DateTimeField(default=timezone.now) #Field to automatically store the timestamp when the task is created
+    updated_at = models.DateTimeField(auto_now=True) #Field to automatically store the timestamp everytime the task is edited
 
+    #To show the task's title
     def __str__(self):
         return self.title
     
--- task_management/tasks/serializers.py
+++ task_management/tasks/serializers.py
@@ -1,7 +1,10 @@
-from rest_framework import serializers
-from .models import Task
+from rest_framework import serializers #For converting Python Objects to JSON and vice versa
+from .models import Task #Importing the task model
 
 class TaskSerializer(serializers.ModelSerializer):
+    """
+    For converting task model into JSON data, validating and converting incoming JSON data into a Task model
+    """
     class Meta:
         model = Task
         fields = '__all__'
